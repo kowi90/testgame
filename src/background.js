3480eaf31998c966da767ba13ac540d9a8b127a1
Gergo Kovacs <gergo.kovacs.90@gmail.com>
2019-07-24 23:46:14 +0200

    working draft

--- src/background.js
+++ src/background.js
@@ -1,14 +1,16 @@
-import {setPixel, createPixelArray, createBufferContext} from './helpers';
-import { SCALE, renderOffset } from './globals';
+import {
+  setPixel,
+  createPixelArray,
+  createBufferContext,
+  randBetween
+} from './helpers';
+import { SCALE } from './globals';
 
-let bufferContext = null;
-let actualBufferData = null;
-let generatedSand = null;
-let generatedGrass = null;
-let generatedSky = null;
+const RANDOMINESS = 10;
+const GROUND_TILE_SIZE = 15;
+const GRASS_TILE_SIZE = 5;
+const SKY_TILE_SIZE = 20;
 
-
-const STAGE_LENGTH_MULTIPLIER = 3;
 const BROWN_COLOR_SET = [
   [255,235,205],
   [255,228,196],
@@ -31,32 +33,62 @@ const BLUE_COLOR_SET = [
   [230,238,245]
 ];
 
-const renderPartial = (context, fullBg, xOffset, yOffset) => {
-  fullBg.forEach(({x, y, color}) => setPixel(x + xOffset, y + yOffset, color, SCALE, context));
+let firstRender = true;
+
+let groundTextures = [];
+let skyTextures = [];
+let grassTextures = [];
+
+let groundImage = null;
+let skyImage = null;
+let grassImage = null;
+
+const generateTexture = (width, height, colorSet, scale) => {
+  const pixelArray = createPixelArray(0, 0, width, height, scale, colorSet);
+  const bufferContext = createBufferContext(width, height);
+  pixelArray.forEach(({x, y, color}) => setPixel(x, y, color, scale, bufferContext));
+  return bufferContext.getImageData(0, 0, width, height);
 };
 
-const renderScene = (context, elements, xOffset, yOffset) => {
-  elements.forEach(element => {
-    renderPartial(context, element, xOffset, yOffset);
-  });  
-}
+const generateTile = (variations, width, height) => {
+  const singleWidth = variations[0].width;
+  const singleHeight = variations[0].height;
+  const neededRows = Math.round(width / singleWidth);
+  const neededColumns = Math.round(height / singleHeight);
+  const bufferContext = createBufferContext(width, height);
+  
+  for(let currentColumn = 0; currentColumn < neededColumns + 1; currentColumn ++) {
+    for (let currentRow = 0; currentRow < neededRows + 1; currentRow ++) {
+      const randomTile = variations[randBetween(0, variations.length - 1)];
+      bufferContext.putImageData(randomTile, singleWidth * currentRow, singleHeight * currentColumn);
+    }
+  }
+  return bufferContext.getImageData(0, 0, width, height);
+};
 
-const getBufferContext = (context) => {
-  bufferContext = createBufferContext(context.canvas.width, context.canvas.height);
-  generatedSand = createPixelArray(0, 85, 100 * STAGE_LENGTH_MULTIPLIER, 15, SCALE, BROWN_COLOR_SET);
-  generatedGrass = createPixelArray(0, 80, 100 * STAGE_LENGTH_MULTIPLIER, 5, SCALE, GREEN_COLOR_SET);
-  generatedSky = createPixelArray(0, 0, 100 * STAGE_LENGTH_MULTIPLIER, 80, SCALE, BLUE_COLOR_SET);
-  renderScene(bufferContext, [generatedSand, generatedGrass, generatedSky], 0, 0);
-  return bufferContext;
+const generateTextures = (randomness) => {
+  for (let i = 0; i < randomness; i++) {
+    groundTextures.push(generateTexture(GROUND_TILE_SIZE, GROUND_TILE_SIZE, BROWN_COLOR_SET, SCALE));  
+    skyTextures.push(generateTexture(SKY_TILE_SIZE, SKY_TILE_SIZE, BLUE_COLOR_SET, SCALE));
+    grassTextures.push(generateTexture(GRASS_TILE_SIZE, GRASS_TILE_SIZE, GREEN_COLOR_SET, SCALE));
+  }
+};
+
+const createTiles = () => {
+  groundImage = generateTile(groundTextures, 1000, 100);
+  grassImage = generateTile(grassTextures, 1000, 20);
+  skyImage = generateTile(skyTextures, 1000, 380);
 };
 
 const render = (context) => {
-  if(!generatedSand) {
-    bufferContext = getBufferContext(context);
-    return;
+  if (firstRender) {
+    generateTextures(RANDOMINESS);
+    createTiles();
+    firstRender = false;
   }
-  actualBufferData = bufferContext.getImageData(0, 0, bufferContext.canvas.width, bufferContext.canvas.height);
-  context.putImageData(actualBufferData, renderOffset, 0);
+  context.putImageData(groundImage, 0, 400);
+  context.putImageData(grassImage, 0, 380);
+  context.putImageData(skyImage, 0, 0);
 };
 
 export default {
